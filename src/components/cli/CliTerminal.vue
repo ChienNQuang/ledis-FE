1b37fb8c660e8f0ca839ea8974faf09ff194fb44
Chien Nguyen Quang <quangchieens1@gmail.com>
2025-06-05 20:08:27 +0700

    feat: implement sendCommand function for CLI input handling

--- src/components/cli/CliTerminal.vue
+++ src/components/cli/CliTerminal.vue
@@ -2,8 +2,34 @@
 import { ref } from 'vue'
 import CliInput from './CliInput.vue'
 import CliOutput from './CliOutput.vue'
+import { SendCommand } from '@/services/commands'
+import { convertToRespStringLines } from '@/lib/resp'
 
 const logs = ref<string[]>([])
+const processing = ref(false)
+
+async function sendCommand(input: string) {
+  logs.value.push(`Ledis> ${input}`)
+  try {
+    processing.value = true
+    if (input.trim() === '') {
+      return
+    }
+
+    const parts = input.split(' ')
+    const command = parts[0]
+    const args = parts.slice(1)
+
+    const res = await SendCommand(command, args)
+
+    const responseLines = convertToRespStringLines(res.data)
+    logs.value.push(...responseLines)
+  } catch (error) {
+    logs.value.push(`Error: ${error instanceof Error ? error.message : 'Unknown error'}`)
+  } finally {
+    processing.value = false
+  }
+}
 </script>
 
 <template>
@@ -12,7 +38,7 @@ const logs = ref<string[]>([])
       class="relative border-[#6b6f24] text-[#d4be98] font-semibold border-2 size-full overflow-auto p-2 no-scrollbar"
     >
       <CliOutput :logs />
-      <CliInput @update="logs.push($event)" />
+      <CliInput v-if="!processing" @update="sendCommand($event)" />
     </div>
   </div>
 </template>
